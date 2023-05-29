f81a78980c3b605b868478cdd4701cebc0214e87
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-29 19:23:54 +0300

    Add files via upload

---
+++ ShpapeProject/Classshape.cs
@@ -0,0 +1,105 @@
+﻿namespace shapeProject
+{
+    public class Shape
+    {
+        int baseTriangular = 0;
+        int NumberSquare = 0;
+        int daimondBase = 0;
+        //constractor
+        public Shape()
+        {
+            baseTriangular = 0;//base of triangle 
+            daimondBase = 0;
+            NumberSquare = 0;// the length of the square
+        }
+
+
+
+        public void Print_Triangular()
+        {
+            Console.WriteLine("enter the base of shape you want");
+            int baseTriangular = int.Parse(Console.ReadLine());
+            string LatterTriangular = "*";
+            if (baseTriangular > 3 && baseTriangular < 10)
+            {
+                for (int i = 0; i < baseTriangular; i++)
+                {
+                    for (int j = baseTriangular - i; j >= 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine(LatterTriangular);
+                    LatterTriangular += "**";
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("low or high number ");
+
+            }
+        }
+
+        //print square
+        public void print_Square()
+        {
+            Console.WriteLine("enter the base of shape you want");
+            int NumberSquare = int.Parse(Console.ReadLine());
+            if (NumberSquare > 3 && NumberSquare < 10)
+            {
+                for (int k = 0; k < NumberSquare; k++)
+                {
+                    for (int s = 0; s < NumberSquare; s++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("low or high number ");
+
+            }
+
+        }
+        //print dimond 
+        public void print_dimond()
+        {
+            Console.WriteLine("enter the base of shape you want");
+            int diamondBase = int.Parse(Console.ReadLine());
+            string diamondLine = "*";
+            if (diamondBase > 3 && diamondBase < 10)
+            {
+                for (int i = 0; i < diamondBase; i++)
+                {
+                    for (int j = diamondBase - i; j >= 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine(diamondLine);
+                    diamondLine += "**";
+                }
+
+                for (int i = diamondBase - 1; i >= 0; i--)
+                {
+                    for (int j = diamondBase - i; j >= 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine(diamondLine.Substring(0, diamondLine.Length - 2));
+                    diamondLine = diamondLine.Substring(0, diamondLine.Length - 2);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("low or high number ");
+
+            }
+
+        }
+    }
+
+}
---
+++ ShpapeProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using shapeProject;
+
+
+class shapes
+{
+    static void Main(string[] args)
+    {
+
+        Shape shape = new Shape();
+
+        int choice = 0;
+        while (choice != 4)
+        {
+            Console.WriteLine(" 1. print a trinagle");
+            Console.WriteLine(" 2. print a square");
+            Console.WriteLine(" 3. print dimond");
+            Console.WriteLine(" 4. exit menu");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+
+                case 1:
+                    shape.Print_Triangular();
+
+                    break;
+                case 2:
+                    shape.print_Square();
+                    break;
+                case 3:
+                    shape.print_dimond();
+
+                    break;
+                case 4:
+                    Console.WriteLine("you done");
+                    break;
+                default:
+                    Console.WriteLine("wrong choice");
+                    break;
+            }
+
+        }
+    }
+
+}
+
