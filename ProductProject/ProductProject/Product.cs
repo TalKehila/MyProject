17334012ac522fa467840d360d8733069a553330
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-29 19:22:56 +0300

    Add files via upload

---
+++ ProductProject/ProductProject/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ProductProject
+{
+    class Product
+    {
+        private string _name = "";
+        private double _price;
+        private DateTime _orginalexpirationDate;
+       
+        public Product(string name, int numday, double price = 100)//defult 100 
+        {
+            
+            SetName(name);
+            SetPrice(price);
+            SetWarrantyExpirationDate(numday);
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetName(string name)
+        {
+            return _name;
+        }
+        public void SetPrice(double price)
+        {
+            if (price > 0)
+            {
+                _price = price;
+            }
+        }
+        public double GetPrice()
+        {
+            return _price;
+        }
+       
+        public void SetWarrantyExpirationDate(int numDays)
+        {
+            if (numDays >= 0)
+            {
+                _orginalexpirationDate = DateTime.Now.AddDays(numDays);
+            }
+        }
+        public string GetFormattedWarrantyExpirationDate()
+        {
+            return $"Warranty expiration date: {_orginalexpirationDate.ToString("dd/MM/yyyy")}";
+        }
+        public DateTime GetWarrantyExpirationDate()
+        {
+            return _orginalexpirationDate;
+        }      
+        public override string ToString( )
+        {
+            double numDays = (int)(_orginalexpirationDate - DateTime.Now).TotalDays;// the exp date- the date time now 
+            DateTime updatedDate = DateTime.Now.AddDays(numDays);// update the date to the update days 
+            int daysLeft = (int)(_orginalexpirationDate - updatedDate).TotalDays;
+            DateTime dt = DateTime.Now;// Globalization
+            string theDate = $"{dt:F}";
+            int days = (int)(_orginalexpirationDate - DateTime.Now).TotalDays;// the days left like the date left 
+            return $"product : {_name}\nprice : {_price}\nthe day today: {(theDate)}\nthe exp date is {(updatedDate)} (left {days} days)";
+        }              
+    }
+}
---
+++ ProductProject/ProductProject/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ProductProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Product[] products = { new Product("milk", 15 , 7), new Product("chicken", 13, 30), new Product("fish", 15, 65) };
+          
+            while (true)
+            {
+                loop(products);
+            }
+        }
+        static void loop(Product[] products)
+        {
+            Console.WriteLine("What is your name : ");// asking for name 
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < products.Length; i++)// printing the product
+            {
+                Console.WriteLine($"[{i + 1}]");
+                Console.WriteLine(products[i]);
+            }
+            Console.WriteLine($"Please choose product (1-{products.Length})");// choosing product
+            int choice = ReadNumber("Please write only numbers, try again: ");
+            while (choice < 1 || choice > products.Length)
+            {
+                Console.WriteLine("Sorry, we can't find this product... try again");// cant find 
+                choice = ReadNumber("Please write only numbers, try again: ");
+            }
+            Console.WriteLine("Thank you, ");
+            Console.WriteLine(name);
+            Console.WriteLine($"\nInvoce:\n{products[choice - 1]}");
+            Console.ReadKey();
+        }
+            static int ReadNumber(string error = "Invalid")//reading the number for product
+            {
+                int result = 0;
+                while (!int.TryParse(Console.ReadLine(), out result))
+                {                   
+                    Console.WriteLine(error);                
+                }
+                return result;
+            }        
+    }
+}
