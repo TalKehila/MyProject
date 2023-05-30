919709b1c1500770b1aa36f8381e9a0bd30e39c2
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-05-30 09:52:35 +0300

    Add files via upload

---
+++ Project number 4/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using Project_number_4;
+
+class program
+{
+    static void Main()
+    {
+        string type = " ";
+       Vehicle[] myvehicles= new Vehicle[4];
+ 
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine("wihch of vehicle you want for  car press 1 , for boat 2 , for truck 3 ");
+            int choiceCarorBoat = int.Parse(Console.ReadLine());
+            if (choiceCarorBoat == 1)
+            {
+                type= " car";
+                myvehicles[i] = new car(type);
+            }
+            else if (choiceCarorBoat == 2)
+            {
+                type = "boat";
+                myvehicles[i] = new boat(type);
+            }
+            else if (choiceCarorBoat == 3)
+            {
+                type = "truck";
+                myvehicles[i] = new Truck(type);
+            }
+           
+            else if (choiceCarorBoat != 1 || choiceCarorBoat != 2 || choiceCarorBoat != 3)
+            {
+                Console.WriteLine("worng choice next time press 1 for car and 2 for boat ");
+            }  
+            
+
+        }
+        for (int i = 0; i < myvehicles.Length; i++)
+        {
+            
+            Console.WriteLine( $"your vehivle  is {type} : " + myvehicles[i]);
+        }
+    }
+
+}
+
+
---
+++ Project number 4/Vehicle.cs
@@ -0,0 +1,94 @@
+﻿using Project_number_4;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Project_number_4
+{
+    class Vehicle
+    {
+
+        private string _make = "";
+        private string _model = "";
+        private string _year = "";
+        private string _nextService = "";
+        public Vehicle(string type)//constrctor 
+        {
+            Console.WriteLine($"make your {type} ,put  value inside");
+            Console.WriteLine("put maker");
+            _make = Console.ReadLine();
+            Console.WriteLine("put model");
+            _model = Console.ReadLine();
+            Console.WriteLine("put year");
+            _year = Console.ReadLine();
+            Console.WriteLine("put Next Service date");
+            _nextService = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $"  make : {_make} model {_model} year {_year} service {_nextService}  ";
+        }
+    }
+    class car : Vehicle
+    {
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        private string _color = "";
+
+        public car(string type) : base(type)
+        {
+            Console.WriteLine("put color for the car ");
+            _color = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $" car color  : {_color}   " + base.ToString();
+        }
+    }
+    class boat : Vehicle
+    {
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _name = "";
+        public boat(string type) : base(type)
+        {
+            Console.WriteLine("put Name for the boat ");
+            _name = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $" Boat name  : {_name}   " + base.ToString();
+        }
+    }
+}
+class Truck : Vehicle
+{
+    public int Weight
+    {
+        get { return _weight; }
+        set { _weight = value; }
+    }
+    private int _weight;
+    public Truck(string type) : base(type)
+    {
+        Console.WriteLine("put weight  for the truck ");
+        _weight = int.Parse(Console.ReadLine());
+    }
+    public override string ToString()
+    {
+        return $" truck weight  : {_weight}   " + base.ToString();
+    }
+}
+
